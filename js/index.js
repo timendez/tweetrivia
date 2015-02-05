f278bd2153542f23cbe9a9225051a83bcd895d0d
ylevitsk <ylevitsk@aol.com>
2015-02-05 00:04:34 -0800

    category menu added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-window.onload = function() {
-    window.setInterval(getTime, 1000);
-    window.setInterval(checkTime, 1000);
-};
+var timeI, checkI;
 
 function getTime(){
    var x = document.getElementById("timer");
@@ -19,15 +16,29 @@ function checkTime(){
    if(x <= 0){
       $("#mask").removeClass("hide");
       $("#popup").removeClass("hide");
+      clearInterval(timeI);
+      clearInterval(checkI);
    }
 }
+function start(){
+   $("#mask2").addClass("hide");
+   $("#popup2").addClass("hide");
+   document.getElementById("timer").innerHTML = 5;
+   timeI = window.setInterval(getTime, 1000);
+   checkI = window.setInterval(checkTime, 1000);
+}
 function change(){
-
+   $("#mask").addClass("hide");
+   $("#popup").addClass("hide");
+   $("#mask2").removeClass("hide");
+   $("#popup2").removeClass("hide");
 }
 function restart(){
-    $("#mask").addClass("hide");
-    $("#popup").addClass("hide");
-    document.getElementById("timer").innerHTML = 5;
+   $("#mask").addClass("hide");
+   $("#popup").addClass("hide");
+   document.getElementById("timer").innerHTML = 5;
+   timeI = window.setInterval(getTime, 1000);
+   checkI = window.setInterval(checkTime, 1000);
 }
 
 function answer(str){
