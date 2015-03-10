a5de8a245983c45d697085b544447eb709f7bb4c
Tim Mendez <hikeemhigh@gmail.com>
2015-03-09 21:34:34 -0700

    sneaky changes test

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -23,7 +23,7 @@ function receiveTopScores(results, category) {
    else {
       for(var i = 0; i < results.length; i++) {
          if(results[i][0] === username) {
-            tableResults += "<tr><td .currentUser=" + username + ">" + results[i][0] + "</td><td .currentUser=" + username + ">" + results[i][1] + "</td></tr>";
+            tableResults += "<tr><td class=\"currentUser\">" + results[i][0] + "</td><td class=\"currentUser\">" + results[i][1] + "</td></tr>";
          }
          else {
             tableResults += "<tr><td>" + results[i][0] + "</td><td>" + results[i][1] + "</td></tr>";
