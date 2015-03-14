c99dddd7c95f8425d3813861b12bc2fd1d704961
Tim Mendez <hikeemhigh@gmail.com>
2015-03-14 00:16:32 -0700

    something

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -15,12 +15,14 @@ function populateLeaderboards() {
 function receiveTopScores(results, category) {
    var tableResults = "";
 
+   //Accessed page not from redirect link
    if(username === undefined) {
       for(var i = 0; i < results.length; i++) {
          tableResults += "<tr><td>" + results[i][0] + "</td><td>" + results[i][1] + "</td></tr>";
       }
    }
    else {
+      //Accessed page from redirect link
       for(var i = 0; i < results.length; i++) {
          if(results[i][0] === username) {
             tableResults += "<tr><td class=\"currentUser\">" + results[i][0] + "</td><td class=\"currentUser\">" + results[i][1] + "</td></tr>";
