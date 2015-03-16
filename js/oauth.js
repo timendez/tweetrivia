10f5dffdd6fd8e3c6a6cd293a3211e872247825c
Varsha Roopreddy <vroopred@calpoly.edu>
2015-03-15 21:25:22 -0700

    Alert of url

--- js/oauth.js
+++ js/oauth.js
@@ -43,6 +43,8 @@ function callback(){
         },
         function (reply) {
             cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+            
+            alert(reply.profile_image_url);
             document.getElementById('username').innerHTML = "Yooo "+ reply.screen_name+"!!!!";
             document.getElementByID('profPic').src =  reply.profile_image_url;
             alert(reply.profile_image_url);
